216f44045e6349fd83e7e7315a95e82c75b92564
JimLee1996 <59061463@qq.com>
2019-01-15 13:26:27 +0800

    luci-app-koolproxy: minor adjustment

--- applications/luci-app-koolproxy/luasrc/controller/koolproxy.lua
+++ applications/luci-app-koolproxy/luasrc/controller/koolproxy.lua
@@ -4,6 +4,6 @@ function index()
 		return
 	end
 
-	entry({"admin","network","koolproxy"},cbi("koolproxy/global"),_("KP"),35).dependent=true
+	entry({"admin","network","koolproxy"},cbi("koolproxy/global"),_("KoolProxy"),35).dependent=true
 	entry({"admin","network","koolproxy","rss_rule"},cbi("koolproxy/rss_rule"), nil).leaf=true
 end
